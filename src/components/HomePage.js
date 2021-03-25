cd892383d2712d5dafe16d0c085525357e2176a8
Dan Paddock <danpaddock@Dans-MacBook-Air.local>
2021-03-25 12:03:46 -0400

    new login page that takes username and password

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,13 @@
-import React from 'react';
-
-import { Link } from 'react-router-dom';
-import './../styles/styles.scss';
+import React from "react";
+import LoginPopup from "./LoginPopup.js";
+import "./../styles/styles.scss";
 
 const HomePage = () => {
-    return (
-    <div className = "_content-container">
-       <div className = "box-layout">
-            <div className="box-layout__box">
-                <h1 className="box-layout__title">Post Board</h1>
-                <p>Welcome, are you a Student or a Teacher?</p>
-                <Link className="button" to="/student">Student</Link>
-                <Link className="button" to="/teacher">Teacher</Link>
-            </div>
-       </div>
+  return (
+    <div className="content-container">
+      <LoginPopup />
     </div>
-    );
-}
+  );
+};
 
 export default HomePage;
